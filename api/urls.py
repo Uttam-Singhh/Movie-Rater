8162d1344d6cfa67f92644ba6c6e1a7b0c14dada
Uttam Singh <uttamkhanduja@yahoo.in>
2021-06-02 02:25:54 +0530

    Added serializers

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
+from api.views import MovieViewSet
 from django import urls
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from .views import MovieViewSet, RatingViewSet
 
 router=routers.DefaultRouter()
+router.register('movie',MovieViewSet)
+router.register('ratings',RatingViewSet)
 
 
 urlpatterns = [
