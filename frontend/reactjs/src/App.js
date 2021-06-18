a9bbb9883f0d62a7341a28e4233d01370aa2b004
Uttam Singh <uttamkhanduja@yahoo.in>
2021-06-19 00:05:20 +0530

    Edit CSS

--- frontend/reactjs/src/App.js
+++ frontend/reactjs/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie Rater</h1>
       </header>
+      <div className="layout">
+        <div>Movie List</div>
+        <div>Movie Details</div>
+      </div>
     </div>
   );
 }
